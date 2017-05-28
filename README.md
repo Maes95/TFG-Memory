fe078964ba5590f153d3da775d0dade44e7931bf
Maes95 <michel.maes95@gmail.com>
2017-05-28 20:00:52 +0200

    Add gemfile

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TFG-Memory
+
+Download dependencies
+
+```sh
+  bundle install
+```
+
+Generate PDF
+
+```sh
+  ./generate.sh
+```
--- generate.sh
+++ generate.sh
@@ -1,1 +1,4 @@
-asciidoctor-pdf -a pdf-stylesdir=resources/themes memoria.adoc
+# Generate PDF
+asciidoctor-pdf -a pdf-stylesdir=resources/themes draft.adoc
+# Merge with cover page
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=memory.pdf cover/cover-page.pdf draft.pdf
