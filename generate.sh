c75303109cb504a03ccd3b0db8deb773e59575ff
Maes95 <michel.maes95@gmail.com>
2017-05-30 18:17:49 +0200

    Intro + Objectives

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 # Generate PDF
-asciidoctor-pdf -a pdf-stylesdir=resources/themes draft.adoc
+asciidoctor-pdf draft.adoc
 # Merge with cover page
 gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=memory.pdf cover/cover-page.pdf draft.pdf
